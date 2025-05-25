c05b60a1de43308e36a2aaffa5b2a0a88c627324
gitautami <utamigita797@gmail.com>
2025-05-25 15:10:17 +0700

    add file cjson

---
+++ cjson/cjson.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
+
+type Mahasiswa struct {
+    Nama   string `json:"nama"`
+	Kelas    string `json:"kelas"`
+    NPM    string `json:"npm"`
+    Jurusan string `json:"jurusan"`
+}
+
+func main() {
+    r := gin.Default()
+
+    r.GET("/datamahasiswa", func(c *gin.Context) {
+        data := Mahasiswa{
+            Nama:   "Resqi Aulia Gita Utami",
+			Kelas:  "2A",
+            NPM:    "714230003",
+            Jurusan: "D4 Teknik Informatika",
+        }
+        c.JSON(http.StatusOK, data) // Ini yang dimaksud "return c.JSON"
+    })
+
+    r.Run(":8080") // Jalankan server di localhost:8080
+}
